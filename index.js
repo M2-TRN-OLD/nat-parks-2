0d6f51ddc069d4064b543fecffb947ad82fcb100
Missy Maloney <missymaloney1@gmail.com>
2018-11-26 08:55:17 -0500

    exercise complete

--- index.js
+++ index.js
@@ -3,6 +3,31 @@
 const natParksUrl = 'https://api.nps.gov/api/v1/parks';
 const apiKey = 'TdqzbQg49evPRDmxgIMNPHDg2bQtOLvudFSyx4QV';
 
+function displayParks(responseJson) {
+    console.log('display parks fired');
+    console.log(`${responseJson.data[1].states}`);
+    console.log(`${responseJson.data.length}`);
+    
+    
+    $('#js-parks-list').empty();
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $('#js-parks-list').append(`<li><h3>${responseJson.data[i].fullName}</h3>
+          <p>${responseJson.data[i].url}</p>
+          <p>${responseJson.data[i].states}</p>
+          </li>`);
+        console.log(`${responseJson.data[i].name}`);
+    }
+    $('.js-results-section').removeClass('hidden');
+
+}
+
+function formatQueryParams(params) {
+    console.log('formatQueryParams fired');
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+      console.log(queryItems.join('&'));
+      return queryItems.join('&');
+}
 
 function getNatParksData(states, maxStates) {
     console.log('getNatParksData fired');
@@ -14,13 +39,28 @@ function getNatParksData(states, maxStates) {
     }
 
     const options = {
-        headers: new Headers {
+        headers: new Headers({
             api_key:apiKey
-        }
+        })
     }
 
-    //const queryParams = formatQueryParams(params);
-    //const url = natParksUrl + '?' + queryParams;
+    const queryParams = formatQueryParams(params);
+    const url = natParksUrl + '?' + queryParams;
+
+    fetch(url)
+      .then (response => {
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error(response.statusText);
+      })
+      .then (responseJson => {
+          console.log(responseJson);
+          displayParks(responseJson);
+      })
+      .catch (err => {
+          $('#js-error-msg').text(`Something went wrong!' ${err.message}`);
+      })
 
 }
 
